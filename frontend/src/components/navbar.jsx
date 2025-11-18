6acb81f08a0edbba59e01b377eb388c3630166e2
yash-sharma-2005 <yashsharmaaa2005@gmail.com>
2025-11-19 01:58:26 +0530

    ...

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import {NavLink} from 'react-router-dom'
+import { Link, NavLink} from 'react-router-dom'
 
 const Navbar = () => {
     return(
@@ -35,7 +35,18 @@ const Navbar = () => {
 
          <div className='group relative'>
             <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+               <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                  <p className='cursor-pointer hover:text-black'>My Profile</p>
+                  <p className='cursor-pointer hover:text-black'>Orders </p>
+                  <p className='cursor-pointer hover:text-black'>Logout </p>
+               </div>
+            </div>
          </div>
+         <Link to='/cart' className='relative'>
+         <img src={assets.cart_icon}  className='w-5 min-w-5' alt="" />
+         <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+         </Link>
        </div>
         </div>
     )
